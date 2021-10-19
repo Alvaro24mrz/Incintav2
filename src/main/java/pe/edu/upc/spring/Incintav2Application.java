7b7480069b53c7b2d238f55584b2a69839dce6a9
Alvaro <alvaro7mrz@gmail.com>
2021-10-18 21:48:01 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/spring/Incintav2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Incintav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Incintav2Application.class, args);
+	}
+
+}
